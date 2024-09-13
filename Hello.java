b712ac519bca273bbdec4eb531afaf1a392cebd0
vladislav1603 <71124578+vladislav1603@users.noreply.github.com>
2024-09-13 12:58:13 +0300

    Завершено задание 8-10

--- Hello.java
+++ Hello.java
@@ -16,23 +16,30 @@ public class Hello {
         numberCheck(roundNumber);
 
         System.out.println(reversCheck(roundNumber));
+
+        cyclePrint("Написать этот текст осталось: ", 5);
+
+        System.out.println(leapCheck(30));
+
+        reversArray();
+
     }
     
     public static void checkSomeSing(int a, int b) {
         int c = a + b;
         if(c >= 0) {
             System.out.println("Сумма положительная");
-        }else{
+        } else {
             System.out.println("Сумма отрицательная");
         }
     }
 
-    public static void printColor(int a){
-        if(a <= 0){
+    public static void printColor(int a) {
+        if(a <= 0) {
             System.out.println("Красный");
-        }else if (a > 1 && a <= 100){
+        } else if (a > 1 && a <= 100){
             System.out.println("Желтый");
-        }else {
+        } else {
             System.out.println("Зелёный");
         };
     }
@@ -45,8 +52,7 @@ public class Hello {
         }
     }
 
-    public static boolean summFilter(int a, int b)
-    {
+    public static boolean summFilter(int a, int b) {
         if (a + b >= 10 && a + b <= 20) {
             return true;
         } else {
@@ -67,6 +73,34 @@ public class Hello {
             return false;
         } else {
             return true;
+        }
+    }
+
+    public static void cyclePrint(String a, int b) {
+        for (int i = 1; i <= b; i++ ) {
+            System.out.println(a + (b-i) + " раз");
+        }
+    }
+
+    public static boolean leapCheck(int a) {
+        if (a < 100 && a % 4 == 0) {
+            return true;
+        } else if (a > 100 && a % 100 == 0 && a % 400 == 0) {
+            return true;
+        } else if (a > 100 && a % 100 != 0 && a % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void reversArray() {
+        int[] binar = {1, 0, 1, 1, 1, 0, 1, 0};
+        for(int i = 0; i < binar.length; i++ ) {
+            if (binar[i] == 1) {
+                binar[i] = 0;
+            } else binar[i] = 1;
+            System.out.println(binar[i]);
         }
     }
 }
