b93773e90f04c916d53bae1ace1280990bf6eb7d
vladislav1603 <71124578+vladislav1603@users.noreply.github.com>
2024-10-01 15:59:10 +0300

    1

---
+++ .vscode/Task1.java
@@ -0,0 +1,24 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int n = 50;
+        double[] array = new double[n];
+        for(int i = 0; i < array.length; i ++){
+            array[i] = Math.random();
+        }
+        double max = array[0];
+        double min = array[0];
+        double avg = 0;
+        for(int i = 0; i < array.length; i ++){
+            if(max < array[i]);{
+                max = array[i];
+            } 
+            if (min > array[i]){
+                min = array[i];
+            }
+            avg += array[i]/array.length;
+        }
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+        System.out.println("avg = " + avg);
+     }
+}
