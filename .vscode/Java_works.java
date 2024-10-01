fba1f6b93e390019a4535b02c3e800cc8a2149b9
vladislav1603 <71124578+vladislav1603@users.noreply.github.com>
2024-10-01 15:24:26 +0300

    1

--- .vscode/Java_works.java
+++ .vscode/Java_works.java
@@ -1,5 +1,9 @@
 public class Java_works {
     public static void main(String[] args) {
+       Arr(args);
+    }
+
+    public static void Arr(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.print(i + " ");
         }
