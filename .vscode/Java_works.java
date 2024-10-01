86632883068df946d0afc848a80458f866c769e9
vladislav1603 <71124578+vladislav1603@users.noreply.github.com>
2024-10-01 15:19:52 +0300

    2

--- .vscode/Java_works.java
+++ .vscode/Java_works.java
@@ -1,4 +1,4 @@
-public class NumbersFromOneToTen {
+public class Java_works {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.print(i + " ");
