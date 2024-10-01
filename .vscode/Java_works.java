9244b454deea615013486f45197d31509e1298ff
vladislav1603 <71124578+vladislav1603@users.noreply.github.com>
2024-10-01 15:28:26 +0300

    2

--- .vscode/Java_works.java
+++ .vscode/Java_works.java
@@ -1,11 +1,20 @@
 public class Java_works {
     public static void main(String[] args) {
        Arr(args);
+       Sum(args);
     }
 
     public static void Arr(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.print(i + " ");
         }
+    }
+
+    public static void Sum(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum +=i;
+        }
+        System.out.println("Сумма чисел от 1 до 100: " + sum);
     }
 }
