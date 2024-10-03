cbeef567a0e2a21cdf4008e590a3ad30ba4d6f68
vladislav1603 <71124578+vladislav1603@users.noreply.github.com>
2024-10-03 15:07:48 +0300

    посик простых чисел

--- .vscode/Task1.java
+++ .vscode/Task1.java
@@ -33,5 +33,17 @@ public class Task1 {
                 }
             }
         }
-     }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if(array[j] > array[j + 1]) {
+                    double temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
 }
