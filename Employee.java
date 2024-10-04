a7bd63b6631da222fc3cf6cab391378f6d2d232a
vladislav1603 <71124578+vladislav1603@users.noreply.github.com>
2024-10-04 13:04:22 +0300

    Перенос задач в разные классы

--- Employee.java
+++ Employee.java
@@ -5,6 +5,7 @@ public class Employee {
     private String email;
     private int salary;
     private int age;
+
     public Employee (String firstName, String lastName, String position, String email, int salary, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -13,27 +14,8 @@ public class Employee {
         this.salary = salary;
         this.age = age;
     }
-
     public static void main(String[] args) {
-        
-    }
-
-    public void printInfo() {
-        System.out.println("ФИО: " + firstName + "" + lastName);
-        System.out.println("Должность: " + position);
-        System.out.println("Почта: " + email);
-        System.out.println("Зарплата: " + salary + " рублей");
-        System.out.println("Возраст: " + age + " лет");
-    }
-
-    public void employeeArray() {
-
-        Employee[] persArray = new Employee[5];
-        persArray[0] = new Employee("Ivan", "Ivanov", "Enginner", "ivanov@yandex.ru", 50000, 23);
-        persArray[1] = new Employee("Jhon", "JavaScript", "IT", "jvIT@gmail.ru", 690000, 41);
-        persArray[2] = new Employee("", "lastName", "position", "email", 502300, 31);
-        persArray[3] = new Employee("", "lastName", "position", "email", 986700, 63);
-        persArray[4] = new Employee("", "lastName", "position", "email", 167800, 83);
-
+        Employee president = new Employee("Дмитрий", "Морозов", "Президент", "morozov@gmail.com", 1500000, 60);
+        System.out.println("ФИО: " + president.firstName + " " + president.lastName + ";" + "" );
     }
 }
---
+++ Person.java
@@ -0,0 +1,14 @@
+
+public class Person {
+
+    public void PersonArray() {
+
+        Person[] persArray = new Employee[5];
+        persArray[0] = new Person("Ivan", "Ivanov", "Enginner", "ivanov@yandex.ru", 50000, 23);
+        persArray[1] = new Person("Jhon", "JavaScript", "IT", "jvIT@gmail.ru", 690000, 41);
+        persArray[2] = new Person("", "lastName", "position", "email", 502300, 31);
+        persArray[3] = new Person("", "lastName", "position", "email", 986700, 63);
+        persArray[4] = new Person("", "lastName", "position", "email", 167800, 83);
+
+    }
+}
