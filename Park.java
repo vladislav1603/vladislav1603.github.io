9205b757f14dba4e5c09829692c1d27bbc21dbc3
vladislav1603 <71124578+vladislav1603@users.noreply.github.com>
2024-10-04 14:42:39 +0300

    задана основа для Задания 3

--- Park.java
+++ Park.java
@@ -1,2 +1,12 @@
 public class Park {
+    private int Employees;
+    public class Attractions {
+        private String name;
+        private int workingHours;
+        private int price;
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
