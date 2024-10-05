44c8a1fb3f6146eaf284233feb32350dce786371
vladislav1603 <71124578+vladislav1603@users.noreply.github.com>
2024-10-05 09:29:21 +0300

    Разработан функционал для Задания 3

--- Park.java
+++ Park.java
@@ -1,12 +1,40 @@
 public class Park {
-    private int Employees;
-    public class Attractions {
+    public static class Attractions {
         private String name;
         private int workingHours;
-        private int price;
+        private double price;
+
+        public Attractions(String name, int workingHours, double price) {
+
+            this.name = name;
+            this.workingHours = workingHours;
+            this.price = price;
+
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getTime() {
+            return workingHours;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
     }
 
     public static void main(String[] args) {
-        
+
+        Park park = new Park();
+
+        Park.Attractions carusel = new Park.Attractions("Карусель", 6, 35);
+        Park.Attractions KolesoObozrenya = new Park.Attractions("Колесо Обозрения", 10, 60);
+
+        System.out.println("Название: " + carusel.getName());
+        System.out.println("Время работы: " + carusel.getTime() + " часов");
+        System.out.println("Цена за билет: " + carusel.getPrice() + " рублей");
     }
 }
